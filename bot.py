1ded3acb1366a56f02b71f3ef10e09110e2da939
Simon Shi <simonsmh@gmail.com>
2018-09-23 17:16:45 +0800

    coolq-rss init

---
+++ bot.py
@@ -0,0 +1,51 @@
+import none
+import getopt
+import sys
+import os
+import json
+from aiocqhttp.message import unescape
+from aiocqhttp.exceptions import Error as CQHttpError
+
+from rss import rss
+
+
+def loadConfig():
+    none.logger.info("Refreshing config...")
+    try:
+        with open(os.path.split(os.path.realpath(__file__))[0] + '/config.json', 'r') as f:
+            config = json.loads(f.read())
+            global URLS
+            URLS = config['URLS']
+            global QQGroup
+            QQGroup = config['QQGroup']
+        none.logger.info("Loaded config, %s, %s", URLS, QQGroup)
+    except:
+        none.logger.error(
+            "Config load failed! Make sure config.json is present.")
+        sys.exit(-1)
+
+
+@none.on_command('echo')
+async def _(session: none.CommandSession):
+    await session.send(session.get_optional('message') or session.current_arg)
+
+
+@none.scheduler.scheduled_job('cron', second='*/30')
+async def _():
+    none.logger.info("Running scheduled Job")
+    loadConfig()
+
+    bot = none.get_bot()
+    tmp = rss().query(URLS)
+    none.logger.info(tmp)
+    for i, post in enumerate(tmp):
+        text = '{}\n发布于 {}\n{}'.format(post.title, ' '.join(post.summary.split()), post.link.replace('/Information/..', ''))
+        try:
+            await bot.send_group_msg(group_id=QQGroup, message=text)
+        except CQHttpError:
+            pass
+
+if __name__ == '__main__':
+    loadConfig()
+    none.init()
+    none.run(host='172.17.0.1', port=8080)
---
+++ rss.py
@@ -0,0 +1,74 @@
+import hashlib
+import json
+import logging
+import os
+import sys
+
+import feedparser
+
+logger = logging.getLogger('rss')
+default_handler = logging.StreamHandler(sys.stdout)
+default_handler.setFormatter(logging.Formatter(
+    '[%(asctime)s %(name)s] %(levelname)s: %(message)s'))
+logger.addHandler(default_handler)
+logger.setLevel(logging.DEBUG)
+
+
+class rss():
+    def __init__(self):
+
+        self.urldict = []
+        try:
+            with open(os.path.split(os.path.realpath(__file__))[0] + '/db.json') as f:
+                self.thread = json.load(f)
+                logger.debug("Loaded DB. Not first run.")
+            self.__init_mark = False
+        except:
+            self.thread = []
+            logger.info("Initialized DB. First run detected!")
+            self.__init_mark = True
+
+    def query(self, urldict):
+        value = []
+        for i, target in enumerate(urldict):
+            logger.debug("Querying %s", target)
+            post = self.process(urldict[i])
+            value.extend(post)
+        return value
+
+    def parse(self, url):
+        pop = feedparser.parse(url)
+        if hasattr(pop, 'bozo_exception'):
+            logger.error("Boom at %s", pop.bozo_exception)
+        else:
+            return pop
+
+    def process(self, url):
+        value = []
+        pop = self.parse(url)
+
+        for i, post in enumerate(pop.entries):
+            hash = hashlib.md5(post.title.encode('utf-8')).hexdigest()
+            if hash not in self.thread:
+                self.thread.append(hash)
+                logger.debug("Getting %s# post at %s", i, url)
+                value.append(post)
+            else:
+                logger.debug("Skipping %s# post at %s", i, url)
+
+        with open('./db.json', 'w+') as f:
+            json.dump(self.thread, f)
+            logger.debug("Stored DB")
+        if self.__init_mark:
+            return []
+
+        return value
+
+
+if __name__ == "__main__":
+    URLS = [
+        "https://www.cnbeta.com/backend.php",
+        "https://www.ithome.com/rss/"
+    ]
+    r = rss().query(URLS)
+    print(r)
